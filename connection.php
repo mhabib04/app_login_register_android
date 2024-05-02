a8acf43c9e7bb30075a9ca68616d72946331b652
mhabib04 <mhabib180404@gmail.com>
2024-05-02 20:00:53 +0700

    first commit

---
+++ connection.php
@@ -0,0 +1,29 @@
+<?php
+
+$connection = null;
+
+try{
+    //Config
+    $host = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "app_login_register_android";
+
+    //Connect
+    $database = "mysql:dbname=$dbname;host=$host";
+    $connection = new PDO($database, $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // if($connection){
+    //     echo "Koneksi Berhasil";
+    // } else {
+    //     echo "Gagal gan";
+    // }
+
+
+} catch (PDOException $e){
+    echo "Error ! " . $e->getMessage();
+    die;
+}
+
+?>
